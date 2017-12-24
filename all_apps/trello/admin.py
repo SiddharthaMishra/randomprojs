c9d5989e5cf356f3374e36f836677a696c4f85ca
Siddhartha_Mishra <sidm1999@gmail.com>
2017-12-24 18:47:40 +0530

    linked templates, created test model objects

--- all_apps/trello/admin.py
+++ all_apps/trello/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Board, Task, Tasklist
+
 # Register your models here.
+
+admin.site.register(Tasklist)
+admin.site.register(Task)
+admin.site.register(Board)
--- all_apps/trello/urls.py
+++ all_apps/trello/urls.py
@@ -5,6 +5,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view, name = 'index'),
-    path('<int:pk>/', views.BoardView.as_view, name = 'board')
+    path('', views.IndexView.as_view(), name = 'index'),
+    path('<int:pk>/', views.BoardView.as_view(), name = 'board')
 ]

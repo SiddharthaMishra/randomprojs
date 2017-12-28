85889cf60383adf603c13c68954a1feab1543c61
SiddharthaMishra <sidm1999@gmail.com>
2017-12-28 22:15:19 +0530

    Cleaned the code

--- all_apps/trello/models.py
+++ all_apps/trello/models.py
@@ -1,3 +1,5 @@
+""" Models """
+
 from django.db import models
 
 # Create your models here.
--- all_apps/trello/views.py
+++ all_apps/trello/views.py
@@ -8,37 +8,50 @@ class IndexView(generic.ListView):
     model = Board
     template_name = 'trello/index.html'
 
+
 class BoardView(generic.DetailView):
     model = Board
     template_name = 'trello/board.html'
 
-def change_pos(request, board_no):
-    board = get_object_or_404(Board, pk=board_no)
-    col = board.tasklist_set.get(pk=int(request.POST['colID'][4:]))
-    for index, taskID in enumerate(request.POST.getlist('item[]')):
-        task = Task.objects.get(pk=taskID) 
-        task.pos_in_list = index
-        task.Tasklist = col
-        task.save()
-    return HttpResponse('done')
 
-def add_task(request, board_no):
+def change_pos(request, board_no):
+    """ To update the new positions of the tasks in the database """
+    
     board = get_object_or_404(Board, pk=board_no)
     col = board.tasklist_set.get(pk=int(request.POST['colID']))
-    pos = col.task_set.all().count()
+
+    for index, taskID in enumerate(request.POST.getlist('item[]')):
+        task = Task.objects.get(pk=taskID) 
+        task.pos_in_list = index    # positions updated according to their index in the sent array
+        task.Tasklist = col     # in case tasks change column
+        task.save()
+
+    return HttpResponse('')
+
+
+def add_task(request, board_no):
+    """ To add new task """
+
+    board = get_object_or_404(Board, pk=board_no)
+    col = board.tasklist_set.get(pk=int(request.POST['colID']))
+    pos = col.task_set.all().count()    # pos_in_list of the new task 
     text = request.POST['text']
     q = Task(Tasklist=col, task_text=text, pos_in_list = pos )
     q.save()
-    return HttpResponse("<div class='task' id='item_"+str(q.id)+"'>"+text+"</div>")
+    return HttpResponse("<div class='task' id='item_" + str(q.id) + "'>" + text + "</div>") # Returns object to be rendered
 
 def active(request, board_no):
+    """ Inverts the active state to the task """
+
     taskno = int(request.POST['pk'])
     q = Task.objects.get(pk=taskno)
     q.task_enabled = not q.task_enabled
     q.save()
-    return HttpResponse("success")
+    return HttpResponse("")
 
 def add_column(request, board_no):
+    """ Adds new tasklist """
+
     board = get_object_or_404(Board, pk=board_no)
     text = request.POST['text']
     q = Tasklist(Board=board, tasklist_name=text)
@@ -46,6 +59,7 @@ def add_column(request, board_no):
     return HttpResponse("<div class='column' id='col_"+str(q.id)+"'><h2 class='listName'>"+text+"</h2></div>")
 
 def add_board(request):
+    """ Adds new Board """
     text = request.POST['text']
     q = Board(board_name=text)
     q.save()
